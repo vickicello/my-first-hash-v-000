6babedea1d84d65b95a2a173f6c20871693d4623
Vicki Hamm <vicki.cello@gmail.com>
2018-06-22 14:51:23 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,12 +26,6 @@ def adding
   shipping_manifest["muskets"] = 2
   shipping_manifest["gun powder"] = 4
 
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
+  shipping_manifest
 
 end
