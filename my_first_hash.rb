c696baa563190eaee300a002dab0e75bc2efde7f
Vicki Hamm <vicki.cello@gmail.com>
2018-06-22 14:47:52 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,20 +1,16 @@
 def my_hash
-  my_hash = {"name" => "Vicki", "age" => "28", "favorite_color" => "goldenrod" }  
+  your_hash = {"name" => "Vicki", "age" => "28", "favorite_color" => "goldenrod" }  
 end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
-
+  the_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3 }
 end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
 
@@ -24,8 +20,8 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
